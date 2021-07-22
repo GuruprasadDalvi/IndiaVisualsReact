83cd0cc29ad8cacea13dcf27067fcfdd2303588e
Guruprasad Dalvi <55258094+GuruprasadDalvi@users.noreply.github.com>
2021-07-22 17:41:46 +0530

    Added few datasets

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   ]
   )
   return (
-    <div className="App">
+    <div className="App" >
       <IndianMap colorFunction={functionHashMap.get(functionCounter)} />
       <button onClick={
         () => {
--- src/Scripts/ColoringFunctions.js
+++ src/Scripts/ColoringFunctions.js
@@ -48,3 +48,27 @@ export function firstTestFunction(idOfState,counter,total) {
     }
     return [0,0,0]
 }
+
+function readFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+
+
+export function lokSabhaMembersStateWise(idOfState){
+    
+}
